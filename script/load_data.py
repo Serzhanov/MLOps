af9af9e463ee43133328f49804ae1ffa49fefe39
Nurbek Serzhanov <ns490367@gmail.com>
2024-03-14 11:54:11 +0100

    reconfig

---
+++ script/load_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-tertiaire-2/lines?size=10000&format=csv&after=10000%2C965634&header=true"
+
+data = pd.read_csv(url)
+data.to_csv("data_ademe.csv", index=False)
+print(data.shape)
